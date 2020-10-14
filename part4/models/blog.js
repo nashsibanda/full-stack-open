6e479b26419540de77183e77f28e10409c005e58
Nash Sibanda <contact@nashsibanda.com>
2020-10-15 00:40:54 +0100

    Complete exercises 4.1-4.2

---
+++ part4/models/blog.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  author: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+});
+
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+module.exports = Blog;
